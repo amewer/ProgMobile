02c39661e1124806569cff6290a804ad0d5884c7
Amélie <amelie.werbregue@etu.univ-tours.fr>
2020-01-30 08:54:31 +0100

    TP2

---
+++ app/src/main/java/com/example/tp2prog/MainActivity.java
@@ -0,0 +1,141 @@
+package com.example.tp2prog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button envoyer = null;
+    Button reset = null;
+    EditText taille = null;
+    EditText poids = null;
+    CheckBox commentaire = null;
+    RadioGroup group = null;
+    TextView result = null;
+    RadioButton meter=null;
+
+    private final String texteInit = "Cliquez sur le bouton « Calculer l'IMC » pour obtenir un résultat.";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // On récupère toutes les vues dont on a besoin
+        envoyer = (Button)findViewById(R.id.calcul);
+        reset = (Button)findViewById(R.id.reset);
+        taille = (EditText)findViewById(R.id.taille);
+        poids = (EditText)findViewById(R.id.poids);
+        commentaire = (CheckBox)findViewById(R.id.commentaire);
+        group = (RadioGroup)findViewById(R.id.group);
+        meter = (RadioButton) findViewById(R.id.radio_metre);
+        result = (TextView)findViewById(R.id.result);
+
+        // On attribue un listener adapté aux vues qui en ont besoin
+        envoyer.setOnClickListener(envoyerListener);
+        reset.setOnClickListener(resetListener);
+        commentaire.setOnClickListener(checkedListener);
+        taille.setOnKeyListener(modifListener);
+        poids.setOnKeyListener(modifListener);
+    }
+
+
+    private View.OnClickListener envoyerListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            //  on récupère la taille
+            String t = taille.getText().toString();
+            // On récupère le poids
+            String p = poids.getText().toString();
+            float tValue = Float.valueOf(t);
+
+            // Puis on vérifie que la taille est cohérente
+            if(tValue <= 0)
+                Toast.makeText(MainActivity.this, "La taille doit être positive", Toast.LENGTH_SHORT).show();
+            else {
+                float pValue = Float.valueOf(p);
+                if(pValue <= 0)
+                    Toast.makeText(MainActivity.this, "Le poids doit etre positif", Toast.LENGTH_SHORT).show();
+                else {
+                    // Si l'utilisateur a indiqué que la taille était en centimètres
+                    // On vérifie que la Checkbox sélectionnée est la deuxième à l'aide de son identifiant
+                    if (group.getCheckedRadioButtonId() == R.id.radio_centimetre) tValue = tValue / 100;
+                    float imc = pValue / (tValue * tValue);
+                    String resultat="Votre IMC est " + imc+" . ";
+
+                    if(commentaire.isChecked()) resultat += interpreteIMC(imc);
+                    result.setText(resultat);
+                }
+            }
+        }
+    };
+
+
+    private View.OnClickListener resetListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            poids.getText().clear();
+            taille.getText().clear();
+            result.setText(texteInit);
+        }
+    };
+
+
+    private View.OnClickListener checkedListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            if(((CheckBox)v).isChecked()) {
+                result.setText(texteInit);
+            }
+        }
+    };
+
+    // Se lance à chaque fois qu'on appuie sur une touche en étant sur un EditText
+    private View.OnKeyListener modifListener = new View.OnKeyListener() {
+        @Override
+        public boolean onKey(View v, int keyCode, KeyEvent event) {
+            String taille2 =taille.getText().toString();
+            if(taille2.contains(".")){
+                if(!meter.isChecked())
+                    meter.setChecked(true);
+            }
+
+            // On remet le texte à sa valeur par défaut
+            result.setText(texteInit);
+            return false;
+        }
+    };
+
+
+
+    private String interpreteIMC(float imc){
+        String interpretation="";
+        if(imc <=16.5) interpretation="Famine";
+        if(imc>16.5 && imc<=18.5) interpretation="Maigreur";
+        if(imc>18.5 && imc<=25) interpretation="Corpulence normale";
+        if(imc>25 && imc<=30) interpretation="Surpoids";
+        if(imc>30 && imc<=35) interpretation="Obésité moyenne";
+        if(imc>35 && imc<=40) interpretation="Obésité sévère";
+        if(imc>=40) interpretation="Obésité morbide ou massive";
+
+
+        return interpretation;
+
+    }
+}
